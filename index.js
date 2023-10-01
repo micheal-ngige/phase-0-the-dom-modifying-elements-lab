e2eb597877fafefc087c0b1917e778f4946adf05
michael <michaelngige@gmail.com>
2023-10-01 08:37:51 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.textContent = 'YOUR-NAME is the champion';
+
+document.body.appendChild(newHeader);
